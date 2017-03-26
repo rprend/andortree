f2fda4090fe1eac558b7a8c7320e4d5a5e63cd78
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2017-03-25 22:57:45 -0400

    Cleanup python

--- pythonDiseaseScript.py
+++ pythonDiseaseScript.py
@@ -1,20 +1,9 @@
 
 # coding: utf-8
 
-# In[1]:
-
+import operator
 import urllib.request
-
-
-# In[2]:
-
 text = urllib.request.urlopen('http://www.nature.com/article-assets/npg/ncomms/2014/140626/ncomms5212/extref/ncomms5212-s4.txt').read().decode()
-
-
-# In[3]:
-
-text[:100]
-
 
 # In[6]:
 
@@ -29,19 +18,9 @@ for l in list1:
     newarray.append(arr)
 
 
-# In[8]:
-
-newarray[:100]
-
-
 # In[50]:
 
 set(list(x[0] for x in newarray))
-
-
-# In[70]:
-
-# {Symptoms : {disease : weight}}
 
 
 dic = {}
@@ -58,38 +37,13 @@ for row in newarray[1:]:
             dic[sym].append([disease,float(weight)])
 
 
-
-
-# In[71]:
-
-idx = max(dic['Pain'][1])
-
-
-# In[72]:
-
-idx
-
-
-# In[73]:
-
-dic['Pain']
-
-
-# In[74]:
-
-sorted(dic['Pain'], key=lambda x: x[1], reverse=True)
-
-
-# In[75]:
-
-dic['Fever']
-
+# sorted(dic['Pain'], key=lambda x: x[1], reverse=True)
 
 # In[76]:
 
 def diseaseFinder(symptoms):
     diseases = {}
-    for symptom in symptoms: 
+    for symptom in symptoms:
         l = dic[symptom]
         for item,weight in l:
             if item in diseases:
@@ -106,7 +60,6 @@ d = diseaseFinder(['Pain','Fever', 'Fatigue', 'Headache', 'Cough'])
 
 # In[78]:
 
-import operator
 sorted_d = sorted(d.items(), key=operator.itemgetter(1),reverse=True)
 
 
@@ -115,12 +68,8 @@ sorted_d = sorted(d.items(), key=operator.itemgetter(1),reverse=True)
 [x[0] for x in sorted_d[:10]]
 
 
-# In[81]:
-
-set(list(x[0] for x in newarray))
 
 
-# In[ ]:
 
 
 
